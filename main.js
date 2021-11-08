46204b4b0eed3749285f43ca633d0797472252e0
Alvaro <alvaroveneg@gmail.com>
2021-11-08 18:14:37 +0100

    add limit json

--- main.js
+++ main.js
@@ -18,7 +18,16 @@ cloudinary.config({
     api_secret: process.env.API_SECRET
 });
 
-app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.use(express.json({
+    limit: '5mb'
+}))
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/characters', CharacterController)
