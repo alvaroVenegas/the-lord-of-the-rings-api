e760e28de00ce821c474dc627504586a5229fdce
Alvaro <alvaroveneg@gmail.com>
2021-11-10 12:01:42 +0100

    changes

--- main.js
+++ main.js
@@ -19,11 +19,16 @@ cloudinary.config({
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+}));
 
 app.use(express.json({
     limit: '5mb'
